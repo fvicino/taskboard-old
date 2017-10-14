7775e85edabe75cf7d29102e1dc6f343ff3adda1
Frank Vicino <fvicino@hotmail.com>
2017-10-14 17:14:15 +1100

    initial commit od original taskboard sourcecode

---
+++ TaskBoard/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.Framework.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System.Collections;
+using TaskBoard.Models;
+using System.Configuration;
+
+namespace TaskBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        const string TFSURL = "http://ap-syd-tfs:8080/tfs/mojo";
+        const string WORKITEMEDITURL = "http://ap-syd-tfs/sites/Mojo/_layouts/tswa/UI/Pages/WorkItems/WorkItemEdit.aspx";
+        const string WORKITEMEDITPARAMSTRING = "?id={0}";
+
+        public ActionResult Index()
+        {
+            List<TaskStickyNote> notes = new List<TaskStickyNote>();
+
+                //connect to tfs server
+                // The url to the tfs server 
+                Uri tfsUri = new Uri(TFSURL);
+
+                // Load the tfs project collection 
+                var mojo = new TfsTeamProjectCollection(tfsUri);
+
+                // Log in to TFS 
+                mojo.EnsureAuthenticated();
+
+                var wit = mojo.GetService<WorkItemStore>();
+
+                Guid qguid = new Guid(ConfigurationManager.AppSettings["QueryGUID"]);
+                var queryDefinition = wit.GetQueryDefinition(qguid);
+
+                string wiql = queryDefinition.QueryText;
+
+                System.Collections.Hashtable context = new System.Collections.Hashtable();
+
+                context.Add("project", queryDefinition.Project.Name);
+
+                var queryReturn = new Query(wit, wiql, context);
+                var wic = queryReturn.RunQuery(); //.RunLinkQuery();
+                
+                foreach (WorkItem wi in wic)
+                {
+                    TaskStickyNote sn = new TaskStickyNote(wi, StateManager.GetState(wi.Id));
+                    sn.EditURL = WORKITEMEDITURL + String.Format(WORKITEMEDITPARAMSTRING, wi.Id.ToString());
+                    notes.Add(sn);
+                }
+
+                notes = notes.OrderBy(x => (x.RemainingTime == "0" && x.AssignedTo == String.Empty ? "" : x.RemainingTime)).OrderBy(x => x.AssignedTo).OrderBy(x => x.State).ToList();
+
+                return View(notes);
+
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public void SaveViewState(int ID, string X, string Y, string Transform)
+        {
+            StateManager.SetState(ID,X,Y,Transform);
+        }
+
+        public ActionResult ClearState() { 
+            StateManager.ClearState();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ TaskBoard/StateManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using TaskBoard.Models;
+
+namespace TaskBoard
+{
+    public static class StateManager
+    {
+        private static string FILENAME = AppDomain.CurrentDomain.BaseDirectory + "App_Data\\ViewState.xml";
+        private static XmlDocument viewState;
+        private static XmlNode root;
+        static StateManager()
+        {
+            viewState = new XmlDocument();
+            viewState.Load(FILENAME);
+            root = viewState.GetElementsByTagName("Root")[0];
+        }
+
+        public static StickyNoteViewState GetState(int ID){
+
+            foreach (XmlNode n in root.ChildNodes) {
+                if (n.InnerText == ID.ToString()) { 
+                    string i=null,j=null;
+                    if (n.Attributes["X"] != null) { i = n.Attributes["X"].Value; };
+                    if (n.Attributes["Y"] != null) { j = n.Attributes["Y"].Value; };
+                    
+
+                    return new StickyNoteViewState() { Transform = n.Attributes["Transform"].Value, X = i, Y = j };
+                }
+            }
+
+            return null;
+        }
+
+        public static void SetState(int ID, string X, string Y, string Transform) 
+        {
+
+            foreach (XmlNode n in root.ChildNodes)
+            {
+                if (n.InnerText == ID.ToString())
+                {
+                    n.Attributes["X"].Value=X;
+                    n.Attributes["Y"].Value=Y;
+                    n.Attributes["Transform"].Value = Transform;
+                    viewState.Save(FILENAME);
+                    return;
+                }
+            }
+            var node = viewState.CreateNode(XmlNodeType.Element, "StickyNote", "");
+            node.InnerText=ID.ToString();
+            
+            var attx = viewState.CreateAttribute("X");
+            attx.Value = X;
+
+            var atty = viewState.CreateAttribute("Y");
+            atty.Value = Y;
+
+            var attt = viewState.CreateAttribute("Transform");
+            attt.Value = Transform;
+
+            node.Attributes.Append(attx);
+            node.Attributes.Append(atty);
+            node.Attributes.Append(attt);
+            
+            root.AppendChild(node);
+
+            viewState.Save(FILENAME);
+        }
+
+        public static void ClearState(){
+        
+             while ( root.ChildNodes.Count>0){
+                 root.RemoveChild(root.ChildNodes[0]);
+             }
+             viewState.Save(FILENAME);
+        }
+    
+    }
+}
